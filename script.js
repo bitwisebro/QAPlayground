08619e1d1970537fdd41a34880b6a6e8e083e7c4
GauravJani <34713448+bitwisebro@users.noreply.github.com>
2023-12-05 22:44:26 +0530

    added reset function

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ function showConfirmation(event) {
 
     // Display a confirmation popup
     alert("Thank you, " + userName + "! Your ticket has been successfully submitted.");
+
+    // Reset the form fields
+    reset();
+}
+
+function reset(){
+    // Reset the form fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.querySelector('input[name="gender"]:checked').checked = false;
+    document.getElementById("subject").value = "general";
+    document.getElementById("message").value = "";
+    document.getElementById("acceptTerms").checked = false;
 }
